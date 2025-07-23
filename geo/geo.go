9d1aa1b29c078f19d79b04ae9f8d8efc6d6ad921
Doniyor <dkuzybaev@yandex.ru>
2025-07-23 21:05:04 +0300

    added a method for checking the available city

--- geo/geo.go
+++ geo/geo.go
@@ -1,8 +1,10 @@
 package geo
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -14,9 +16,16 @@ type GeoData struct {
 	City string `json:"city"`
 }
 
-func GetMyLocation(city string) (*GeoData, error) {
+type CityPopulationResponse struct {
+	Error bool `json:"error"`
+}
 
-	if len(city) != 0 {
+func GetMyLocation(city string) (*GeoData, error) {
+	
+	if city != "" {
+		if isCity := checkCity(city); isCity {
+			panic("There is no such city!")
+		}
 		return &GeoData{
 			City: city,
 		}, nil
@@ -35,7 +44,7 @@ func GetMyLocation(city string) (*GeoData, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 		return nil, errors.New("NOT-200")
 	}
@@ -56,4 +65,32 @@ func CheckEnv(path string) (string, error) {
 
 	envVar := os.Getenv(path)
 	return envVar, nil
+}
+
+
+func checkCity(city string) (bool) {
+	cityUrl, err := CheckEnv("CITY_POPLR")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	postBody, _ :=json.Marshal(map[string]string{
+		"city": city,
+	})
+
+	resp, err := http.Post(cityUrl, "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+
+	var populationResponse CityPopulationResponse
+	json.Unmarshal(body, &populationResponse)
+	return populationResponse.Error
+
 }
